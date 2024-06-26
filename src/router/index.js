33b9b86d67c9c161137dcae00c2cede2e937a710
Ikhizamah Oyinifeoluwakan <jewelhckr@gmail.com>
2024-06-26 11:25:07 +0100

    not

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import RecipePage from "../views/RecipePage.vue"
 import HomePage from "../views/HomePage.vue";
 import AboutPage from "@/views/AboutPage.vue";
+import ProductPage from "@/views/ProductsPage";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -14,6 +16,9 @@ const router = createRouter({
         {
         path:'/recipe', name: 'foodlist', component: RecipePage,
         },
+        {
+          path: '/products', name: 'productlist', component: ProductPage,
+        }
     ],
 });
 export default router;
